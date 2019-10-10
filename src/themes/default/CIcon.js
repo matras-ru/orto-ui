5378fc745a5499a92d1cf57bc14794047e5863f4
snezzzzhok <zhupikova@matras.ru>
2019-10-10 16:19:36 +0300

    FRONTEND-505 refactor tailwind config and add button component

---
+++ src/themes/default/CIcon.js
@@ -0,0 +1,9 @@
+const Icon = {
+    baseClass: 'inline-block',
+    defaultClass: 'fill-default',
+    defaultSizeClass: 'w-14 h-14',
+    largeSizeClass: '',
+    smallSizeClass: ''
+};
+
+export default Icon;
