c453e30b2ee1c4431b7d4fc717d2a03edb64cbff
sinevich valeriy <sinevich@matras.ru>
2020-02-05 18:33:19 +0300

    FRONTEND-736

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,13 @@ module.exports = {
     rules: {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'vue/no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+        'vue/no-unused-vars': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto'
+            }
+        ]
     },
     parserOptions: {
         parser: 'babel-eslint'
